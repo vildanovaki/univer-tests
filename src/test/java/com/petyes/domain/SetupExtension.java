9c2b6d9f1a771dbb1c3ac0da58a5404987a821de
Камиля <vildanova-kamilya99@yandex.ru>
2023-03-23 00:59:44 +0400

    added fix

--- src/test/java/com/petyes/domain/SetupExtension.java
+++ src/test/java/com/petyes/domain/SetupExtension.java
@@ -10,6 +10,7 @@ import com.petyes.config.App;
 import com.petyes.helpers.DriverSettings;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SetupExtension extends BaseSetupExtension {
     @Override
@@ -20,14 +21,14 @@ public class SetupExtension extends BaseSetupExtension {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://pet-yes.com";
         RestAssured.baseURI = "https://pet-yes.com";
-//        DataBuilder dataBuilder = new DataBuilder();
-//        dataBuilder.setBreederToken();
-//        dataBuilder.setBreederId();
-//        dataBuilder.setCustomerToken();
-//        dataBuilder.setCustomerId();
-//        dataBuilder.setPetId();
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
-//
+
     @Override
     public void close() throws Throwable {
     }
